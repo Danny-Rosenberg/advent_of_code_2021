692899c0bd905b5f9267a4d0148519cc70e042eb
Daniel Rosenberg <rosenbergd36@gmail.com>
2021-12-18 16:30:51 +0000

    solution for day 5 part 2

--- day_5/vents.rb
+++ day_5/vents.rb
@@ -1,4 +1,4 @@
-class Vents1
+class Vents
 	require_relative 'file_parser'
 
 	# indices of single set of hydrothermal vents (a 4 element array)
@@ -7,28 +7,31 @@ class Vents1
 	X2 = 2
 	Y2 = 3
 
-	# directions
-  HOR = 'hor'
+	HOR = 'hor'
 	VER = 'ver'
+
+	ADD = '+'
+	SUB = '-'
 
 	# lines where at least n lines overlap
 	OVERLAP = 2
 
-	attr_reader :lines, :graph
+	attr_reader :lines, :graph, :with_diagonal
 
-	def initialize
+	def initialize(with_diagonal = false)
 		@lines =  FileParser.parse
 
 		dimension = lines.flatten.max + 1
 		@graph = Array.new(dimension) { Array.new(dimension, 0) }
+		@with_diagonal = with_diagonal
 	end
 
 
-	def mark_graph
+	def find_overlaps
 		lines.each do |line|
 		  increment_graph_at_points(line)
 		end
-		puts "The number of overlaps of at least #{OVERLAP} is #{calculate_overlaps}"
+		calculate_overlaps
 	end
 
 
@@ -45,19 +48,33 @@ private
 
 	def marks_for(line)
 		if direction_for(line) == HOR
-			start = ([line[X1], line[X2]]).min
+			start  = ([line[X1], line[X2]]).min
 			finish = ([line[X1], line[X2]]).max
 			(start..finish).map do |i|
 				[i, line[Y1]]
 			end
 		elsif direction_for(line) == VER
-			start = ([line[Y1], line[Y2]]).min
+			start  = ([line[Y1], line[Y2]]).min
 			finish = ([line[Y1], line[Y2]]).max
 			(start..finish).map do |i|
 				[line[X1], i]
 			end
-		else
-			[]
+		else # it is diagonal
+			if with_diagonal
+				length = length_of_diagonal(line)
+
+				# determine direction of line
+				x_operator = line[X1] > line[X2] ? SUB : ADD
+				y_operator = line[Y1] > line[Y2] ? SUB : ADD
+
+				marks = []
+				(0..length).each do |i|
+					marks << [line[X1].send(x_operator, i), line[Y1].send(y_operator, i)]
+				end
+				marks
+			else
+				[]
+			end
 		end
 	end
 
@@ -84,6 +101,14 @@ private
 		end
 	end
 
+
+	def length_of_diagonal(line)
+		(line[X1] - line[X2]).abs
+	end
+
 end
 
-Vents1.new.mark_graph
+vent = Vents.new
+puts "The number of at least 2 overlaps without diagonals: #{vent.find_overlaps}"
+vent = Vents.new(with_diagonal: true)
+puts "The number of at least 2 overlaps including diagonals: #{vent.find_overlaps}"
