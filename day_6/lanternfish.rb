6264ddd1f3defc0e6a25c039cd2c9fba3923e08c
Daniel Rosenberg <rosenbergd36@gmail.com>
2021-12-20 23:25:16 +0100

    start of day 6 part 2, but too slow. Slows down around day 180

--- day_6/lanternfish.rb
+++ day_6/lanternfish.rb
@@ -12,23 +12,40 @@ class Lanternfish
 
 
 	def number_of_fish_after(days)
-		(1..days).each do
-			decrement_values
+		# calculate lifetime for each possible starting value
+		age_descendants = {}
+		(1..5).each do |i|
+			age_descendants[i] = descendants_for_fish_at(i, days).length
 		end
-		values.length
+
+		puts "how do our descendants look? #{age_descendants}"
+
+		calculate_fish_total(age_descendants)
 	end
 
 
-	def decrement_values
-		# update values in place
-		(0...values.length).each do |i|
-			if values[i] == 0
-				values[i] = apply_birth_rule
-				values << new_fish_rule
-			else
-				values[i] -= 1
-			end
+	def calculate_fish_total(age_descendants_hash)
+		values.sum do |v|
+			age_descendants_hash[v]
 		end
+	end
+
+
+	def descendants_for_fish_at(age, days)
+		fish_arr = [age]
+		(1..days).each do |d|
+			(0...fish_arr.length).each do |i|
+				if fish_arr[i] == 0
+					fish_arr[i] = apply_birth_rule
+					fish_arr << new_fish_rule
+				else
+					fish_arr[i] -= 1
+				end
+			end
+			puts "end of day: #{d}"
+			puts "fish so far: #{fish_arr.length}"
+		end
+		fish_arr
 	end
 
 
@@ -48,3 +65,5 @@ require_relative '../utils/file_parser'
 values = FileParser.new('day_6_input.txt').parse_single_row_csv
 number_fish = Lanternfish.new(values).number_of_fish_after(80)
 puts "number of fish after 80 days: #{number_fish}"
+number_fish = Lanternfish.new(values).number_of_fish_after(256)
+puts "number of fish after 256 days: #{number_fish}"
