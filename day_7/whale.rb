05cc2152f057cef8d21227b463f239e65daffa19
Daniel Rosenberg <rosenbergd36@gmail.com>
2021-12-24 22:54:02 +0100

    solution for day 7 part 2

--- day_7/whale.rb
+++ day_7/whale.rb
@@ -1,10 +1,25 @@
 class Whale
 	require_relative '../utils/file_parser'
 
-	attr_reader :values
+	attr_reader :values, :strategy
 
-	def initialize(values)
-		@values = values
+
+	def initialize(values, strategy)
+		incremental_strategy = Proc.new do |crab_positions, position|
+			crab_positions.sum { |c| (c - position).abs }
+		end
+
+
+		increasing_strategy = Proc.new do |crab_positions, position|
+			# (n^2 + n) / 2
+			crab_positions.sum do |c|
+			  sum = (c - position).abs
+				((sum * sum) + sum) / 2
+			end
+		end
+
+		@values   = values
+		@strategy = strategy == :incremental ? incremental_strategy : increasing_strategy
 	end
 
 
@@ -12,11 +27,11 @@ class Whale
 		max = values.max
 		min = values.min
 
-		min_fuel = max * max
-		min_position = max + 1
+		min_fuel = max * 1000000000 # placeholder
+		min_position = -1 # placeholder
 
 		(min..max).each do |c|
-			fuel = fuel_spent(values, c)
+			fuel = strategy.call(values, c)
 			if fuel < min_fuel
 				min_fuel = fuel
 				min_position = c
@@ -27,19 +42,29 @@ class Whale
 	end
 
 
-	def fuel_spent(crab_positions, position)
-		crab_positions.sum { |c| (c - position).abs }
-	end
-
-
 	def self.parse_file
 		FileParser.new('day_7_input.txt').parse_single_row_csv
 	end
 
 end
 
+
 values = Whale.parse_file
 # values = [16,1,2,0,4,2,7,1,2,14]
-results = Whale.new(values).find_horizontal_position
+
+# part 1
+# go through each integer between min and max in the given range
+# check the distance of each value in data set from the current value
+# o(n^2)
+
+results = Whale.new(values, :incremental).find_horizontal_position
+puts "using the 'incremental' strategy, the fuel spent is #{results[0]}"
 puts "the cheapest position is: #{results[1]}"
-puts "the fuel spent is #{results[0]}"
+
+# part 2
+# go through each integer between min and max in the given range
+# check distance using 'addition factorial'
+# o(n^2)
+results2 = Whale.new(values, :increasing).find_horizontal_position
+puts "using the 'increasing' strategy, the fuel spent is #{results2[0]}"
+puts "the cheapest position is: #{results2[1]}"
