6ca80ae815dbd07067b677271ca3cc1d097dd1d4
Daniel Rosenberg <rosenbergd36@gmail.com>
2022-01-05 10:08:19 -0500

    create separate class for decrypting signals

--- day_8/seven_segment.rb
+++ day_8/seven_segment.rb
@@ -1,5 +1,14 @@
 class SevenSegment
 
+	#			  [0]
+	#				 -
+	#	 [1] |   | [2]
+	#			  [3]
+	#			   -
+	#	 [4] |   | [5]
+	#			   -
+	#       [6]
+
 	attr_reader :signals_output
 
 	def initialize(signals_output)
@@ -18,9 +27,17 @@ class SevenSegment
 	end
 
 
+	def add_output_values
+		signals_output.sum do |signals, output|
+			decrypted_array = SignalDecrypter.new(signals).decrypt_signals
+			# decrypt outputs
+		end
+	end
+
+
 private
 
-
+	# solution 1
 	def decrypt_one_output(single_output)
 		results = value_checks.map do |func|
 			func.call(single_output)
@@ -30,6 +47,7 @@ private
 	end
 
 
+	# used in solution 1
 	def value_checks
 		@value_checks ||= begin
 			[
@@ -41,6 +59,82 @@ private
 		end
 	end
 
+
+	class SignalDecrypter
+
+		attr_reader :signals
+
+		def initialize(signals)
+			@signals = signals
+		end
+
+
+		def decrypt_signals(signals)
+			signal_array = [-1, -1, -1, -1, -1, -1, -1]
+
+			signal_array[0] = find_zero_index(one_signal, seven_signal)
+			signal_array[2], signal_array[4], signal_array[5] = find_two_four_five_indices(signals, one_signal)
+			signal_array[1] = find_one_index(signals, signal_array)
+		end
+
+
+		def one_signal
+			@one_signal ||= signals.find { |sig| sig.length == 2 }
+		end
+
+
+		def seven_signal
+			@seven_signal ||= signals.find { |sig| sig.length == 3 }
+		end
+
+
+		def find_six_and_nine
+			@six_and_nine_signals ||= signals.filter { |sig| sig.length == 6 }
+		end
+
+
+		def three
+			@three ||= \
+				two_three_five_signals.filter { |sig| (sig.split('') - one_signal.split('')).length == 3 }
+		end
+
+
+		def two_three_five_signals
+			@two_three_five_signals = signals.filter { |sig| sig.length == 5 }
+		end
+
+
+		def six
+			@six ||= \
+				(six_and_nine_signals[0].split('') - one_signal.split('')).length == 5 ? six_and_nine_signals[0] : six_and_nine_signals[1]
+		end
+
+
+		def nine(signals)
+			@nine ||= \
+				six_and_nine = find_six_and_nine_signals(signals)
+				six(signals) == six_and_nine[0] ? six_and_nine[1] : six_and_nine[0]
+		end
+
+
+		def find_zero_index
+			# 'dab' - 'ab' = 'd'
+			seven_signal.split('') - one_signal.split('').first
+		end
+
+
+		def find_one_index(signals, signal_array)
+
+		end
+
+
+		def find_two_four_five_indices
+			four_index = (six.split('') - (nine.split('') - one_signal.split(''))).first
+			two_index = (one_signal.split('') - six.split('')).first
+			five_index = (one_signal.split('') - [two_index]).first
+			return two_index, four_index, five_index
+		end
+	end
 end
 
 
